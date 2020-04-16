cac20075e6a3fa9e478ab6d67d06edbc714389a3
issuy <akito.ew@gmail.com>
2020-04-16 20:12:06 +0900

    Define procedure.

---
+++ draw.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("[Information]\n")
+	fmt.Printf("Rarity & Rate:\n")
+	fmt.Printf("Rarity & Item:\n")
+	fmt.Printf("[Execute]\n")
+	fmt.Printf("Draw\n")
+	fmt.Printf("[Result]\n")
+	fmt.Printf("Rarity:---\n")
+	fmt.Printf("Item:---\n")
+}
