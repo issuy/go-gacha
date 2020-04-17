8794011ca2fc012ebbd8e6a60b73ff87e65ec852
issuy <akito.ew@gmail.com>
2020-04-17 22:33:52 +0900

    Definition of rarity and probability.

--- draw.go
+++ draw.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type Rarity struct {
+	id   int
+	name string
+	rate int // 万分率
+}
+
 func main() {
 	fmt.Printf("[Information]\n")
 	fmt.Printf("Rarity & Rate:\n")
@@ -11,4 +17,12 @@ func main() {
 	fmt.Printf("[Result]\n")
 	fmt.Printf("Rarity:---\n")
 	fmt.Printf("Item:---\n")
+}
+
+func GetRarities() []Rarity {
+	return []Rarity{
+		{1, "R", 6000},
+		{2, "SR", 3500},
+		{3, "UR", 500},
+	}
 }
